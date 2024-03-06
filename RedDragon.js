75125d5332aca03916bbfd09923c068637d6930c
aruvoke <84821114+aruvoke@users.noreply.github.com>
2024-03-07 03:16:23 +0700

    Create RedDragon.js

---
+++ RedDragon.js
@@ -0,0 +1,19 @@
+{
+  "name": "RedDragon",
+  "version": "1.0.0",
+  "description": "utility tool",
+  "main": "reddragon.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/aruvoke/RedDragon.git"
+  },
+  "author": "Aruvoke",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/aruvoke/RedDragon/issues"
+  },
+  "homepage": "https://github.com/aruvoke/RedDragon#readme"
+}
